e26b2ab202721b84b6d8b07e14bae2afad26244c
tata32000 <tata32000@hotmail.com>
2025-02-26 21:10:55 +0800

    Add endpoint for esp32 testing

---
+++ src/app/api/esp32/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  const data = await request.json();
+  return NextResponse.json({
+    message: "received: " + JSON.stringify(data),
+  });
+}
+
+export async function GET(_request: Request) {
+  return NextResponse.json({
+    message: "Test api endpoint for ESP32",
+    status: "OK",
+  });
+}
