731787ecc40a049f1103c08301392ca5005b3ecf
tata32000 <tata32000@hotmail.com>
2025-02-27 15:58:00 +0800

    Add reload button to log

--- src/app/log/page.tsx
+++ src/app/log/page.tsx
@@ -3,8 +3,8 @@ import LogTable from "./_components/log-table";
 export default function DashboardPage() {
   return (
     <>
-      <h1 className="m-10 text-4xl font-bold">Dashboard</h1>
-      <div className="m-10 flex h-screen flex-col items-center">
+      <h1 className="m-10 -mb-10 text-4xl font-bold">Log Dashboard</h1>
+      <div className="m-10 flex h-screen flex-col">
         <LogTable />
       </div>
     </>
